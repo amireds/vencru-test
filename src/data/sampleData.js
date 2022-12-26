6a48ad38988cd27b618d7c0f6503cb564ebfb03c
Amire Steve <zoe_steve@hotmail.com>
2022-12-26 12:37:59 +0100

    Moved data and functions

---
+++ src/data/sampleData.js
@@ -0,0 +1,271 @@
+import {
+  BarChart2,
+  CheckSquare,
+  Flag,
+  Home,
+  Layers,
+  LifeBuoy,
+  Settings,
+  Users,
+  Key,
+} from "feather-icons-react";
+
+export const navigation = [
+  { name: "Home", href: "#", icon: Home, current: true, isDivider: true },
+  {
+    name: "Dashboard",
+    href: "#",
+    icon: BarChart2,
+    current: false,
+    isDivider: true,
+    count: 10,
+  },
+  {
+    name: "Projects",
+    href: "#",
+    icon: Layers,
+    current: false,
+    isDivider: true,
+  },
+  {
+    name: "Tasks",
+    href: "#",
+    icon: CheckSquare,
+    current: false,
+    isDivider: true,
+  },
+  {
+    name: "Reporting",
+    href: "#",
+    icon: Flag,
+    current: false,
+    isDivider: true,
+  },
+  {
+    name: "Users",
+    href: "#",
+    icon: Users,
+    current: false,
+    isDivider: true,
+  },
+  { name: "", href: "#", icon: Key, current: false, isDivider: false },
+  {
+    name: "Support",
+    href: "#",
+    icon: LifeBuoy,
+    current: false,
+    isDivider: true,
+  },
+  {
+    name: "Settings",
+    href: "#",
+    icon: Settings,
+    current: false,
+    isDivider: true,
+  },
+];
+
+export const plans = [
+  {
+    invoice: "basic plan - dec 2022",
+    amount: "USD $10.00",
+    date: "dec 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Dries Vincent",
+        email: "dries.vincent@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Lindsay Walton",
+        email: "lindsay.walton@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Courtney Henry",
+        email: "courtney.henry@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Tom Cook",
+        email: "tom.cook@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 7,
+  },
+  {
+    invoice: "basic plan - nov 2022",
+    amount: "USD $10.00",
+    date: "nov 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Whitney Francis",
+        email: "whitney.francis@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Leonard Krasner",
+        email: "leonard.krasner@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Floyd Miles",
+        email: "floy.dmiles@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 10,
+  },
+  {
+    invoice: "basic plan - oct 2022",
+    amount: "USD $10.00",
+    date: "oct 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Emily Selman",
+        email: "emily.selman@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Kristin Watson",
+        email: "kristin.watson@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Emma Dorsey",
+        email: "emma.dorsey@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 12,
+  },
+  {
+    invoice: "basic plan - sep 2022",
+    amount: "USD $10.00",
+    date: "sep 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Dries Vincent",
+        email: "dries.vincent@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Lindsay Walton",
+        email: "lindsay.walton@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Courtney Henry",
+        email: "courtney.henry@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Tom Cook",
+        email: "tom.cook@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 6,
+  },
+  {
+    invoice: "basic plan - aug 2022",
+    amount: "USD $10.00",
+    date: "aug 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Whitney Francis",
+        email: "whitney.francis@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Leonard Krasner",
+        email: "leonard.krasner@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Floyd Miles",
+        email: "floy.dmiles@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 12,
+  },
+  {
+    invoice: "basic plan - jul 2022",
+    amount: "USD $10.00",
+    date: "jul 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Emily Selman",
+        email: "emily.selman@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Kristin Watson",
+        email: "kristin.watson@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Emma Dorsey",
+        email: "emma.dorsey@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 8,
+  },
+  {
+    invoice: "basic plan - jun 2022",
+    amount: "USD $10.00",
+    date: "jun 1, 2022",
+    status: "paid",
+    users: [
+      {
+        name: "Emily Selman",
+        email: "emily.selman@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Kristin Watson",
+        email: "kristin.watson@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        name: "Emma Dorsey",
+        email: "emma.dorsey@example.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+    totalUsers: 10,
+  },
+  // More Plans...
+];
